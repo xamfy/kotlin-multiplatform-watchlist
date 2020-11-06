da0ffa60c09fb702de0ad4fd6583065c0e012d29
xamfy <spratyaksh22@gmail.com>
2020-11-06 22:39:37 +0530

    Initial commit

---
+++ KmmApp/shared/src/commonMain/kotlin/com/xamfy/myapplication/shared/Greeting.kt
@@ -0,0 +1,8 @@
+package com.xamfy.myapplication.shared
+
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ KmmApp/shared/src/commonMain/kotlin/com/xamfy/myapplication/shared/Platform.kt
@@ -0,0 +1,5 @@
+package com.xamfy.myapplication.shared
+
+expect class Platform() {
+    val platform: String
+}
