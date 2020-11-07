65c6e6b08f2dcb1c3feefb194a0b90b4b55ff51e
xamfy <spratyaksh22@gmail.com>
2020-11-07 15:59:18 +0530

    Create SDK class

---
+++ Kmm/shared/src/commonMain/kotlin/com/xamfy/kmm/shared/SpaceXSDK.kt
@@ -0,0 +1,23 @@
+package com.xamfy.kmm.shared
+
+import com.xamfy.kmm.shared.cache.Database
+import com.xamfy.kmm.shared.cache.DatabaseDriverFactory
+import com.xamfy.kmm.shared.entity.RocketLaunch
+import com.xamfy.kmm.shared.network.SpaceXApi
+
+class SpaceXSDK (databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = Database(databaseDriverFactory)
+    private val api = SpaceXApi()
+
+    @Throws(Exception::class) suspend fun getLaunches(forceReload: Boolean): List<RocketLaunch> {
+        val cachedLaunches = database.getAllLaunches()
+        return if (cachedLaunches.isNotEmpty() && !forceReload) {
+            cachedLaunches
+        } else {
+            api.getAllLaunches().also {
+                database.clearDatabase()
+                database.createLaunches(it)
+            }
+        }
+    }
+}
