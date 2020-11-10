2bda5a28e3d5841739cf868af16807f25632a5db
xamfy <spratyaksh22@gmail.com>
2020-11-10 10:35:13 +0530

    Add Movies adapter and item_movie layout

---
+++ KmmWatchlist/androidApp/src/main/java/com/xamfy/kmm/androidApp/MoviesRvAdapter.kt
@@ -0,0 +1,40 @@
+package com.xamfy.kmm.androidApp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.xamfy.kmm.shared.entity.Movie
+
+class MoviesRvAdapter(var movies: List<Movie>) :
+    RecyclerView.Adapter<MoviesRvAdapter.MovieViewHolder>() {
+
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val movieImageView = itemView.findViewById<ImageView>(R.id.movieImage)
+        private val movieTitleTextView = itemView.findViewById<TextView>(R.id.movieTitle)
+        private val movieDescTextView = itemView.findViewById<TextView>(R.id.movieDesc)
+        private val movieGenreTextView = itemView.findViewById<TextView>(R.id.movieGenre)
+
+        fun bindData(movie: Movie) {
+            movieImageView.load(movie.imageUrl)
+            movieTitleTextView.text = movie.title
+            movieDescTextView.text = movie.description
+            movieGenreTextView.text = movie.genre
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        return LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_movie, parent, false)
+            .run(::MovieViewHolder)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bindData(movies[position])
+    }
+
+    override fun getItemCount(): Int = movies.count()
+}
