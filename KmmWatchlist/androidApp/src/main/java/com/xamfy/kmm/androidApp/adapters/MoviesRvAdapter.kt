22379b7b36268a9bcea65212cb6f1c6637b45fe6
xamfy <spratyaksh22@gmail.com>
2020-11-12 19:09:20 +0530

    Add popup menu for movie list item

--- KmmWatchlist/androidApp/src/main/java/com/xamfy/kmm/androidApp/adapters/MoviesRvAdapter.kt
+++ KmmWatchlist/androidApp/src/main/java/com/xamfy/kmm/androidApp/adapters/MoviesRvAdapter.kt
@@ -1,10 +1,13 @@
 package com.xamfy.kmm.androidApp.adapters
 
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.xamfy.kmm.androidApp.R
@@ -13,11 +16,17 @@ import com.xamfy.kmm.shared.entity.Movie
 class MoviesRvAdapter(var movies: List<Movie>) :
     RecyclerView.Adapter<MoviesRvAdapter.MovieViewHolder>() {
 
-    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener,
+        PopupMenu.OnMenuItemClickListener {
         private val movieImageView = itemView.findViewById<ImageView>(R.id.movieImage)
         private val movieTitleTextView = itemView.findViewById<TextView>(R.id.movieTitle)
         private val movieDescTextView = itemView.findViewById<TextView>(R.id.movieDesc)
         private val movieGenreTextView = itemView.findViewById<TextView>(R.id.movieGenre)
+        private val optionsImageView = itemView.findViewById<ImageView>(R.id.moreVertIcon)
+
+        init {
+            optionsImageView.setOnClickListener(this)
+        }
 
         fun bindData(movie: Movie) {
             movieImageView.scaleType = ImageView.ScaleType.CENTER_CROP
@@ -28,6 +37,29 @@ class MoviesRvAdapter(var movies: List<Movie>) :
             movieTitleTextView.text = movie.title
             movieDescTextView.text = movie.description
             movieGenreTextView.text = movie.genre
+        }
+
+        override fun onClick(v: View?) {
+            if (v != null) {
+                showPopupMenu(v)
+            }
+        }
+
+        private fun showPopupMenu(view: View) {
+            val popupMenu = PopupMenu(view.context, view)
+            popupMenu.inflate(R.menu.list_item_popup_menu)
+            popupMenu.setOnMenuItemClickListener(this)
+            popupMenu.show()
+        }
+
+        override fun onMenuItemClick(item: MenuItem?): Boolean {
+            return when(item?.itemId) {
+                R.id.action_add_to_watchlist -> {
+                    Toast.makeText(itemView.context, movieTitleTextView.text, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
     }
 
