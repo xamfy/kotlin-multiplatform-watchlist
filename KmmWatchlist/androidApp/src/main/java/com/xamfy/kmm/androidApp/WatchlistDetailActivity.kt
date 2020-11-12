642a003f71eba12cee5d65ac87145d1efec9de21
xamfy <spratyaksh22@gmail.com>
2020-11-12 15:19:07 +0530

    Create WatchlistDetailActivity

---
+++ KmmWatchlist/androidApp/src/main/java/com/xamfy/kmm/androidApp/WatchlistDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.xamfy.kmm.androidApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WatchlistDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_watchlist_detail)
+    }
+}
